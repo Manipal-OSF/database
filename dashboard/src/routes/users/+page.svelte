b8d2b174aa28a7c04b9bd88571abf529a118d66d
dhzdhd <72333092+dhzdhd@users.noreply.github.com>
2023-02-09 16:31:36 +0530

    Initialise dashboard subproject (#6)
    * Add keygen functionality
    
    * SHA based auth system
    
    * Add protected routes
    
    * Add JWT
    
    * Add working auth and user route
    
    * Refactor auth models
    
    * Add update_user endpoint and debug keys
    
    * Fix update endpoint
    
    * Add create endpoint
    
    * Improve error handlers
    
    * Update branch
    
    * Update error message
    
    * Initialise sveltekit with tailwindcss and daisyUI
    
    * Format code

---
+++ dashboard/src/routes/users/+page.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<div class="overflow-x-auto">
+	<table class="table w-full">
+		<!-- head -->
+		<thead>
+			<tr>
+				<th />
+				<th>{data.registrationNumber}</th>
+				<th>Job</th>
+				<th>Favorite Color</th>
+			</tr>
+		</thead>
+		<tbody>
+			<!-- row 1 -->
+			<tr>
+				<th>1</th>
+				<td>Cy Ganderton</td>
+				<td>Quality Control Specialist</td>
+				<td>Blue</td>
+			</tr>
+			<!-- row 2 -->
+			<tr class="hover">
+				<th>2</th>
+				<td>Hart Hagerty</td>
+				<td>Desktop Support Technician</td>
+				<td>Purple</td>
+			</tr>
+			<!-- row 3 -->
+			<tr>
+				<th>3</th>
+				<td>Brice Swyre</td>
+				<td>Tax Accountant</td>
+				<td>Red</td>
+			</tr>
+		</tbody>
+	</table>
+</div>
---
+++ dashboard/src/routes/users/+page.ts
@@ -0,0 +1,11 @@
+import type { PageLoad } from './$types';
+
+export const ssr = true;
+
+export interface UserModel {
+	registrationNumber: number;
+}
+
+export const load: PageLoad = async () => {
+	return { registrationNumber: 2 } satisfies UserModel;
+};
